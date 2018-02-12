2e59997e4c8fc587c9def8b0c221faaaff774bab
Akshith Gunasekaran <gunaseka@oregonstate.edu>
2018-02-12 13:38:58 -0800

    add comments to BPVOnline for better understanding

--- FourQ_64bit_and_portable/bpv.c
+++ FourQ_64bit_and_portable/bpv.c
@@ -5,55 +5,78 @@
 #include <malloc.h>
 #include <string.h>
 
-#define BPV_n 1024 //
-#define BPV_k 16   //
+// bpv_n and bpv_k are the bpv generator params
+// k subsets over n random numbers alpha_i
+#define BPV_n 1024
+#define BPV_k 16
+
+// Algo: (x, g^x) for Schnorr
+//
+// A) Preprocessing step:
+// Generate bpv_n random integers alpha_i
+// Compute beta_i = g ^ alpha_i
+// Store both in a table
+//
+// B) When a pair (x, g^x) is needed:
+// Randomly select S subset [1, bpv_n] and |S| = bpv_k
+// x = Sum_(i in S) of alpha_i mod ord(g) (THIS CAN BE PARALLELIZED)
+//     if x = 0 stop and start again
+// g^x = Product_(i in S) of beta_i (THIS CAN BE PARALLELIZED)
+// return (x, g^x)
 
 ECCRYPTO_STATUS BPVOnline(const unsigned char *SecretKey, const unsigned char *PublicKey, unsigned char *lastSecret, unsigned char *lastPublic)
 {
-    ECCRYPTO_STATUS Status = ECCRYPTO_ERROR_UNKNOWN;
-    digit_t *S = (digit_t *)(lastSecret);
-    unsigned char *index;                         //
+    ECCRYPTO_STATUS Status = ECCRYPTO_ERROR_UNKNOWN; // initially no error
+    digit_t *S = (digit_t *)(lastSecret); //
+    unsigned char *index; // bpv_n random numbers
     unsigned char secretTemp[32], publicTemp[64]; //
-    unsigned char secretTemp2[32];                //
-    int k;                                        // loop var, [0, BPV_k -2)
+    unsigned char secretTemp2[32]; //
+    int k; // loop variable
 
     //point_t Temp;
-    point_t Added;                          //
-    point_extproj_t AddedExtproj;           //
-    point_extproj_t TempExtproj;            //
+    point_t Added; //
+    point_extproj_t AddedExtproj; //
+    point_extproj_t TempExtproj; //
     point_extproj_precomp_t TempExtprojPre; //
 
+    // Precomputation
+    // Generate bpv_n random numbers and put it in index
     Status = RandomBytesFunction(index, BPV_n);
     if (Status != ECCRYPTO_SUCCESS)
     {
         goto cleanup;
     }
 
-    index[0] = BPV_n * index[0] / 256;
+    // obtain (x, g^x)
+    // A i=0
+    index[0] = BPV_n * index[0] / 256; //
     memmove(secretTemp, SecretKey + 32 * index[0], 32);
     memmove(publicTemp, PublicKey + 64 * index[0], 64);
 
+    // B i=0
     point_setup((point_affine *)publicTemp, AddedExtproj);
 
+    // A i=1
     index[1] = BPV_n * index[1] / 256;
     memmove(secretTemp2, SecretKey + 32 * index[1], 32);
     memmove(publicTemp, PublicKey + 64 * index[1], 64);
 
+    // B i=1
     point_setup((point_affine *)publicTemp, TempExtproj);
     R1_to_R3(TempExtproj, TempExtprojPre);
-
     eccadd(TempExtprojPre, AddedExtproj);
     add_mod_order((digit_t *)secretTemp, (digit_t *)secretTemp2, S);
 
     for (k = 0; k < BPV_k - 2; k++)
     {
+        // A i=2 to bpv_k
         index[k + 2] = BPV_n * index[k + 2] / 256;
         memmove(secretTemp, SecretKey + 32 * index[k + 2], 32);
         memmove(publicTemp, PublicKey + 64 * index[k + 2], 64);
 
+        // B i=2 to bpv_k
         point_setup((point_affine *)publicTemp, TempExtproj);
         R1_to_R3(TempExtproj, TempExtprojPre);
-
         eccadd(TempExtprojPre, AddedExtproj);
         add_mod_order((digit_t *)secretTemp, S, S);
     }
